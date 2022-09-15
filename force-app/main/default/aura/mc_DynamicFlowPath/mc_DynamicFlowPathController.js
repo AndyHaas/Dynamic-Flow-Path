5d827580653902909c3fffe6163be04e04ecba2a
Andy Haas <andyhaas@MacBook-Pro-2.local>
2022-09-14 20:50:23 -0500

    added click handling on the path component to navigate to the next screen

---
+++ force-app/main/default/aura/mc_DynamicFlowPath/mc_DynamicFlowPathController.js
@@ -0,0 +1,64 @@
+({
+    doInit : function(component, evt, helper) {
+        var currentStep = component.get('v.currentStep');
+        var currentStepPercentage = component.get('v.currentStepPercentage');
+        var stepList = component.get('v.stepList').split(/,/);
+        var cmps = [];
+        var afterCurrent = 'false';
+        var countToCurrent = 0;
+        var countTotalSteps = stepList.length;
+        
+        for (var i=0; i<stepList.length; i++){
+            
+            if(afterCurrent == 'false') {
+                // this step might be Completed or Current
+                if(stepList[i] == currentStep) {
+                    // this is the current step
+                    cmps.push({
+                        'label': stepList[i],
+                        'status': 'Current'
+                    });
+                    
+                    // set afterCurrent to true,
+                    // so all subsequent steps
+                    // are marked as future
+                    afterCurrent = 'true';
+                    countToCurrent++;
+                }
+                else {
+                    // this is a completed step
+                    cmps.push({
+                        'label': stepList[i],
+                        'status': 'Complete'
+                    });
+                    countToCurrent++;
+                }
+            }
+            else {
+                // this is an upcoming step
+                cmps.push({
+                    'label': stepList[i],
+                    'status': 'Upcoming'
+                });
+            }
+        }
+        
+        var pathProgress = (((countToCurrent-1)/(countTotalSteps-1)*100));
+        
+        if(currentStepPercentage) {
+            pathProgress = currentStepPercentage;
+        }
+        
+        component.set("v.countToCurrent", countToCurrent);
+        component.set("v.countTotalSteps", countTotalSteps);
+        component.set("v.pathProgress", pathProgress);
+        component.set("v.stepsArray", cmps);
+    },
+
+    handleOnClick : function(component, event, helper) {
+        console.log('event id', event.target.id);
+        component.set("v.selectedStep", event.target.id);
+        var navigate = component.get("v.navigateFlow");
+        navigate("NEXT");
+    }
+})
